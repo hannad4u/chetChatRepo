1efe9c5bda6dd8a59663e901c26a39ccc0522bf2
hannad4u <hannad4u@gmail.com>
2018-10-20 14:21:31 +0200

    after some edit but it crashed

--- chetChat/Controller/MessageVC.swift
+++ chetChat/Controller/MessageVC.swift
@@ -37,6 +37,39 @@ class MessageVC: UIViewController ,UITableViewDelegate ,UITableViewDataSource ,U
             loadData()
             
         }
+        func keyboardWillShow(notify : NSNotification){
+            
+            if let keyboardSize = (notify.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                if self.view.frame.origin.y == 0{
+                    self.view.frame.origin.y -= keyboardSize.height
+                }
+            }
+        }
+        
+        func keyboardWillHide(notify : NSNotification){
+            
+            if let keyboardSize = (notify.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+                if self.view.frame.origin.y != 0{
+                    self.view.frame.origin.y += keyboardSize.height
+                }
+            }
+        }
+        
+        func dismissKeyboard(){
+            view.endEditing(true)
+        }
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        DispatchQueue.main.asyncAfter(deadline: .now() + milliseconds(300)){
+            self.movToBottom()
+            
+        }
+        
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -47,12 +80,48 @@ class MessageVC: UIViewController ,UITableViewDelegate ,UITableViewDataSource ,U
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let message = messages[indexPath.row]
-        if let cell = tableView.dequeueReusableCell(withIdentifier: "Message") as? MesaagesCell{
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "Message") as? MessagesCell{
             cell.configCell(message: message)
             return cell
+            
+            
         }else{
             return MessagesCell()
         }
+    }
+    
+    func loadData(){
+        Database.database().reference().child("messages").child(messageId).observe(.value, with: { (snapshot) in
+            
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot]{
+                self.messages.removeAll()
+                
+                for data in snapshot{
+                    if let postDict = data.value as? Dictionary<String,AnyObject>{
+                        let key = data.key
+                        let post = Message(messageKey: key, postData: postDict)
+                        self.messages.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
+            
+            
+        })
+    }
+    
+    func movToBottom(){
+        
+        if messages.count > 0{
+            let indexPath = IndexPath(row: messages.count-1, section: 0)
+            
+            tableView.scrollToRow(at: indexPath, at: .bottom, animated: false)
+        }
+    }
+
+    @IBAction func sendPressed(_ sender: AnyObject){
+        
+        
     }
     
 }
--- chetChat/View/MessagesCell.swift
+++ chetChat/View/MessagesCell.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
 
 class MessagesCell: UITableViewCell {
+    
+    @IBOutlet weak var recievedMessageLable : UILabel!
+    @IBOutlet weak var recievedMessageView : UIView!
+    @IBOutlet weak var sentMessageLable :  UILabel!
+    @IBOutlet weak var sentMessageView : UIView!
+    
+    var message : Message!
+    var curruntUser = KeychainWrapper.standard.string(forKey: "uid")
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -19,6 +28,22 @@ class MessagesCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    func configCell(message : Message){
+        self.message  = message
+        
+        if message.sender == curruntUser{
+            sentMessageView.isHidden = false
+            sentMessageLable.text = message.message
+            recievedMessageLable.text = ""
+            recievedMessageLable.isHidden = true
+        } else {
+            sentMessageView.isHidden = true
+            sentMessageLable.text = ""
+            recievedMessageLable.text = message.message
+            recievedMessageLable.isHidden = false
+        }
     }
 
 }
